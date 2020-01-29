0f912e47f9123305174978fa8d1f71f879b31a56
michaelhsladetrng <51245469+michaelhsladetrng@users.noreply.github.com>
2020-01-29 16:37:29 +0000

    done with processor

---
+++ ReservationProcessor/ReservationListener.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RabbitMqUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ReservationProcessor
+{
+    public class ReservationListener : RabbitListener
+    {
+
+        private readonly ILogger<ReservationListener> Logger;
+        private readonly ReservationsHttpService Service;
+
+        public ReservationListener(ReservationsHttpService service, ILogger<ReservationListener> logger, IOptionsMonitor<RabbitOptions> options) : base(options)
+        {
+
+            Logger = logger;
+            base.QueueName = "reservationQueue";
+            base.ExchangeName = "";
+            Service = service;
+
+        }
+
+        public override async Task<bool> Process(string message)
+        {
+            var request = JsonSerializer.Deserialize<ReservationRequest>(message);
+            Logger.LogInformation($"Got a Reservation! {Environment.NewLine} \t {request.For}");
+            return await Service.MarkReservationAccepted(request); // Task.FromResult(true);
+        }
+    }
+
+
+    public class ReservationRequest
+    {
+        public int Id { get; set; }
+        public string For { get; set; }
+        public string Status { get; set; }
+        public DateTime ReservationCreated { get; set; }
+        public string[] books { get; set; }
+    }
+
+}
---
+++ ReservationProcessor/ReservationsHttpService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ReservationProcessor
+{
+    public class ReservationsHttpService
+    {
+        private HttpClient Client;
+
+        public ReservationsHttpService(HttpClient client, IConfiguration config)
+        {
+            client.BaseAddress = new Uri(config.GetValue<string>("apiUrl"));
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("User-Agent", "reservation-processor");
+
+            Client = client;
+        }
+
+        public async Task<bool> MarkReservationAccepted(ReservationRequest reservation)
+        {
+            var reservationAsString = JsonSerializer.Serialize(reservation);
+            var content = new StringContent(reservationAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var response = await Client.PostAsync("/reservations/approved", content);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
